61cf71cf1f3d34f9f19a5de662ed7d9c2346322c
Tw0_notebook <romdmitrbsk1701@gmail.com>
2022-03-28 09:43:09 +0300

    Исправлен мертвый код

--- part1/practice/bad_smell_doubles/main.py
+++ part1/practice/bad_smell_doubles/main.py
@@ -10,16 +10,6 @@ class SomeClass:
     def __init__(self):
         self.lst = [3, 2, 1, 4, 2, 1]
 
-    def sorted(self):
-        self.lst.sort()
-        return self.lst
-
-    def sorting(self):
-        return sorted(self.lst)
-
-    def asc_sorting(self):
-        return sorted(self.lst, reverse=False)
-
     def sorted_func(self):
         if type(self.lst) == dict:
             self.lst.sort()
