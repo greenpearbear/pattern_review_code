7c64f8b50a58a3f24bbcf717dc48add1598917d5
Tw0_notebook <romdmitrbsk1701@gmail.com>
2022-03-28 10:09:22 +0300

    Сделан абстрактный класс.

--- part1/practice/abstract_classes/main.py
+++ part1/practice/abstract_classes/main.py
@@ -38,9 +38,76 @@
 # экземпляр класса Person должен поочередно взаимодействовать с объектами Car, Boat, Electroscooter
 
 # код должен выполняться не выбрасывая исключений
+from abc import ABC, abstractmethod
 
-# TODO напишите Ваш код здесь
 
+class Transport(ABC):
+
+    @abstractmethod
+    def start_engine(self):
+        pass
+
+    @abstractmethod
+    def move(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+    @abstractmethod
+    def stop_engine(self):
+        pass
+
+
+class Boat(Transport):
+    def start_engine(self):
+        print('Катер громко затарахтел')
+
+    def move(self):
+        print('Катер быстро набирает скорость')
+
+    def stop(self):
+        print('Катер остановился')
+
+    def stop_engine(self):
+        print('Двигатель катера чихнул напоследок и заглох')
+
+
+class Car(Transport):
+    def start_engine(self):
+        print('Машина заурчала двигателем')
+
+    def move(self):
+        print('Машина едет к цели назначения')
+
+    def stop(self):
+        print('Машина остановилась')
+
+    def stop_engine(self):
+        print('Машина стоит с заглушенным двигателем')
+
+
+class Electroscooter(Transport):
+    def start_engine(self):
+        print('Мигнул светодиодом')
+
+    def move(self):
+        print('Прохожие в ужасе разбегаются от очередного камикадзе')
+
+    def stop(self):
+        print('Торможение об стену прошло успешно')
+
+    def stop_engine(self):
+        print('Мигнул светодиодом дважды')
+
+
+class Person:
+    def use_transport(self, transport):
+        transport.start_engine()
+        transport.move()
+        transport.stop()
+        transport.stop_engine()
 
 # Отрезок кода для самопроверки.
 # Запустите его, после того как выполните задание
